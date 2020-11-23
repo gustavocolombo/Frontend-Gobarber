6b66263eb564f21b989b5077972291425b068a99
Gustavo Colombo <gusttavocolombo@gmail.com>
2020-11-23 20:50:24 -0300

    adicionando estilo global a aplicação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App = () => { 
   return(
-    <h1>initial commit</h1>
+    <>
+      <h1>initial commit</h1>
+      <GlobalStyle/>
+    </>
   )
 };
 
